b83198c9b564b601b07a8cf7a84f21f9364c89ad
dioaroinc <me@dioaro.com>
2021-12-02 16:58:11 +0900

    2nd

--- www/application/controllers/Klass.php
+++ www/application/controllers/Klass.php
@@ -8,33 +8,34 @@ class Klass extends CI_Controller {
 		$this -> load -> model('common_model');
 		$this -> load -> helper(array('url', 'date', 'form'));
 	}
-	
+
 	public function index(){
-		
-	
+
+
 		$data['now_loc'] = "class";
 		$data['now_title'] = "클래스";
-		
-			
+		$data['now'] = "";
+
+
 		$this->load->view('layout/header', $data);
 		$this->load->view('jsc/index', $data);
 		$this->load->view('klass/index', $data);
-		$this->load->view('layout/footer', $data);					
+		$this->load->view('layout/footer', $data);
 
-	}	
-	
+	}
+
 	public function use(){
-		
-	
+
+
 		$data['now_loc'] = "use";
 		$data['now_title'] = "포장";
-		
-			
+
+
 		$this->load->view('layout/header', $data);
 		$this->load->view('jsc/index', $data);
 		$this->load->view('klass/use', $data);
-		$this->load->view('layout/footer', $data);					
+		$this->load->view('layout/footer', $data);
 
-	}		
-		
+	}
+
 }
